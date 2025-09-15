4573818ebf41f65afedefba4ff2271cdd1cf8186
thanhmp2803 <thanhmp2803@gmail.com>
2025-09-15 14:00:00 +0700

    config: i18n

---
+++ src/locales/en/translation.ts
@@ -0,0 +1,7 @@
+const en = {
+  welcome: 'Welcome to my app',
+  hello: 'Hello {{name}}',
+  change_language: 'Change language',
+}
+
+export default en
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@assets/styles/index.css'
 import { App } from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './i18n'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
